c2a84a82d876f9c4527cc3daa04185b13af9e1d7
AlcionePereira <90060154+AlcionePereira@users.noreply.github.com>
2021-09-08 22:28:31 -0300

    Add files via upload

---
+++ seg.meugato.on.py
@@ -0,0 +1,10 @@
+print(" Meu animal favorito é o gato")
+print("/\ "*2)
+print(":* * :")
+print(":`^´ " + "######## + ~~~~~~~")
+print(" \__/" + "#######")
+print(" II " + "~~~II~~")
+
+
+      
+      
